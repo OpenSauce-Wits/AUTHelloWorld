8ee0ae1212b3423956f66d5ac2a649a4ee825ffd
Tieho <1908649@students.wits.ac.za>
2020-03-03 16:49:46 +0200

    Added button and edit text. Modified code to print message on button click

--- app/src/main/java/com/example/authelloworld/MainActivity.java
+++ app/src/main/java/com/example/authelloworld/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.authelloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //adding components on screen
+        final EditText addTextBox = (EditText)findViewById(R.id.ADD_TEXT);
+        final TextView showText = (TextView)findViewById(R.id.SHOW_TEXTVIEW);
+        //add listener for button here
+        Button addTextBut = (Button)findViewById(R.id.SHOW_BUTTON);
+
+        addTextBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showText.setText(addTextBox.getText());
+            }
+        });
     }
 }
